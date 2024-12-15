0aed7246d97cc6d741e547bb90cd1af8b967d965
Meagan Carlesso <meagan.carlesso@gmail.com>
2024-12-15 22:16:05 +0900

    Set up base project

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # CakeDevTask
+
+## Instructions
+
+To run the dev server, run the following command in the terminal:
+
+```
+npm run dev
+```
+
+The app will be running on `http://localhost:3000/`.
+
+## Assumptions
+### Part 1
+### Part 2
+ - Limiting interactivity to navigating component (Not including "see all" button or clicking on individual cards)
+ - Hard coded data
+ - Isolated component - i.e. The rest of the dashboard will not 
+ - mobile version has sligntly different design to maximise vertical screen
+
+## Decisions
+1. Next js web framework. 
+2. Typescript.
+3. 
+4. 
+
+## Plan
+
+- [x] Create react app with template - Next.js
+- [x] Add simple project structure
+    - [x] components
+    - [x] hooks
+    - [x] utils
+- Part 1
+    - [x] Layout simple page structure with components and placeholder text (Responsive)
+    - [x] Add components (semi-functional)
+        - [x] toggle
+        - [x] modal
+        - [x] button
+    - [x] Add CSS and assets
+    - [x] Add functionality
+        - [x] button click
+        - [x] light/dark mode on
+            - [x] toggle
+            - [x] modal
+        - [] Children counter component
+    - [] Bonus: Extend Modal (come back after part 2)
+    - [] Bonus: Confetti Easter Egg (come back after part 2)
+- Part 2
+![Component Layout Diagram](/app/public/Cake%20Dev%20Carousel%20Component%20Diagram.png)
+    - [] Layout component tile (Responsive)
+    - [] Layout card components (Responsive)
+    - [] Styling and Assets
---
+++ app/src/pages/index.tsx
@@ -0,0 +1,61 @@
+import Head from "next/head";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import { AppHeader } from "@/components/AppHeader/AppHeader";
+import { Toggle } from "@/components/Toggle/Toggle";
+import { Button } from "@/components/Button/Button";
+import { useState } from "react";
+import { Modal } from "@/components/Modal/Modal";
+
+const inter = Inter({
+    variable: "--font-inter",
+    subsets: ["latin"],
+});
+
+export default function Home() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
+    return (
+        <>
+            <Head>
+                <title>Cake Dev Task</title>
+                <meta name="description" content="Engineering Task for Cake Interview " />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            {/* App Header */}
+            <AppHeader />
+            <div className={`${styles.page} ${inter.variable} `}>
+                <main className={styles.main}>
+                    {/* Text Container */}
+                    <h2>Welcome</h2>
+                    <div className={styles.content}>
+                        <h5>Change the mood</h5>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non mi eu
+                            massa imperdiet aliquam.
+                        </p>
+                    </div>
+                    {/* Toggle */}
+                    <Toggle />
+                    {/* Modal */}
+                    <Button title="Open Modal" onClick={() => openModal()} variant="primary" />
+                    <Modal
+                        title={"Hello, I'm a modal"}
+                        subtitle={"By clicking the switch, you will toggle the mood"}
+                        isModalOpen={isModalOpen}
+                        onClose={closeModal}
+                    />
+                </main>
+            </div>
+        </>
+    );
+}
